7891e42bcb7be8991a49e8297e1064d1fa6accd8
Jose Gomez <jose.gomez@epsilon.com>
2022-05-25 10:59:50 -0600

    RecipeIngredientEdit Component name correction

--- src/components/RecipeIngredientEdit.js
+++ src/components/RecipeIngredientEdit.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function (props) {
+export default function RecipeIngredientEdit(props) {
   const { ingredient, handleIngredientChange, handleIngredientDelete } = props
 
   function handleChange(changes) {
